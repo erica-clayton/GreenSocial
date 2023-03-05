62181e71882a9db863f3e863187e3607022a357e
erica-clayton <erica.baertsch@gmail.com>
2023-03-05 12:02:44 -0600

    up and running

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -3,13 +3,11 @@ import "./posts.scss"
 const Posts = () => {
     return (
         <div className="posts">
-            {Posts.map(post=>(
+            
                 <div className="post">
-                    {post.map(post=>(
-                        <Post post={post} key={post.id}/>
-                    ))}
+                   
                 </div>
-            ))}
+            
         </div>
     )
 }
